2f3b3c8f7985e9e88279d248f4972f60dbf4e93d
Nadim <nhsajib316@gmail.com>
2023-02-22 01:21:03 +0600

    local authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
+import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { LocalAuthStrategy } from './local-auth-strategy';
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthService],
+  imports: [UsersModule,PassportModule],
+  providers: [AuthService, LocalAuthStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
