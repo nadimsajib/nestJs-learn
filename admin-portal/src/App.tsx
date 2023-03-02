2a4da2038a74a89ca3797f618a6bed586b341975
Nadim <nhsajib316@gmail.com>
2023-03-03 00:55:59 +0600

    user login loggout with react and nest

---
+++ admin-portal/src/App.tsx
@@ -0,0 +1,13 @@
+import { Admin  } from 'react-admin';
+import { useAuthProvider } from "./hooks";
+
+function App() {
+  const {authProvider} = useAuthProvider();
+  return (
+    <Admin authProvider={authProvider} requireAuth>
+      {/** */}
+    </Admin>
+  );
+}
+
+export default App;
